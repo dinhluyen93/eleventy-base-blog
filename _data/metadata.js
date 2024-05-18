ce194482f9cda09275eefae60adaf598e5aa87f0
Zenith <dinhluyenbk93@gmail.com>
2024-05-19 03:23:37 +0700

    assets

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
+	title: "GMS",
 	url: "https://example.com/",
-	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	language: "vi",
+	description: "Phân tích và tối ưu hiệu quả hoạt động cho đối tác XanhSMBike.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
+		name: "Zenith",
+		email: "zenithsociu@gmail.com",
 		url: "https://example.com/about-me/"
 	}
 }
